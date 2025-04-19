694a5589dc48cf26a3bb37bcb41b06771961e807
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-04-19 17:57:44 +0700

    Latihan JS dasar

--- Week - 2/script.js
+++ Week - 2/script.js
@@ -1,9 +1,21 @@
-let nama = "Reza";
-let umur = 23;
-let aktif = true;
+let username = "Reza";
+let usia = 23;
 
-console.log("Nama:", nama);
-console.log("Umur", umur);
-console.log("Status Aktif:", aktif);
+if (usia >= 18) {
+    console.log(username + " Sudah boleh membuat SIM ");
+} else {
+    console.log(username + " Belum cukup usia untuk membuat SIM ");
+}
 
+let status = "admin";
 
+switch (status) {
+    case "admin":
+        console.log("Akses Penuh");
+        break;
+    case "user":
+        console.log("Akses Terbatas");
+        break;
+    default:
+        console.log("Tidak Dikenal");
+}
