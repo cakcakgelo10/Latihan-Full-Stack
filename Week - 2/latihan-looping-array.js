b623dc4e59e9ce744dcf1995d257ddff27b33a31
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-04-20 20:13:43 +0700

    Latihan  for dan array

---
+++ Week - 2/latihan-looping-array.js
@@ -0,0 +1,18 @@
+// let nama = "Reza";
+// let skill = ["HTML", "CSS", "JavaScript", "Node.js"];
+
+// console.log("skill " + nama + ":");
+
+// for (let i = 0; i < skill.length; i++) {
+//     console.log("- " + skill[i]);
+// }
+
+let nama = "Reza";
+let skill = ["HTML", "CSS", "JavaScript", "Node.js"];
+
+console.log("Skill " + nama + " :");
+
+for (let item of skill) {
+    console.log("- " + item);
+    
+}
