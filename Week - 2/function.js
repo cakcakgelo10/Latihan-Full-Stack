2a15460987e76b970636f794d795144334afc27f
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-04-21 21:13:32 +0700

    Function

---
+++ Week - 2/function.js
@@ -0,0 +1,35 @@
+// 🔸 1. Apa itu Function?
+// Function adalah sekelompok kode yang diberi nama dan bisa dipanggil kapan saja.
+// Contoh:
+// function sapa() {
+//     console.log("Halo, selamat datang!");
+//   }
+  
+//   sapa(); // memanggil function
+ 
+// 🔸 2. Function dengan Parameter
+// Function bisa menerima input (parameter):
+// function sapaUser(nama) {
+//     console.log("Halo, " + nama + "!");
+//   }
+  
+//   sapaUser("Reza");  // Output: Halo, Reza!
+//   sapaUser("Dian");  // Output: Halo, Dian!
+
+// 🔸 3. Function dengan Return
+// Function bisa mengembalikan nilai (return):
+// function jumlah(a, b) {
+//     return a + b;
+//   }
+  
+//   let hasil = jumlah(10, 5);
+//   console.log("Hasil:", hasil); // 15
+
+// 🔸 4. Arrow Function (Versi Singkat)
+// Arrow function = cara modern menulis function (dipakai banget di JavaScript modern seperti React).
+// const kali = (a, b) => {
+//     return a * b;
+//   };
+  
+//   console.log(kali(4, 5)); // 20
+  
