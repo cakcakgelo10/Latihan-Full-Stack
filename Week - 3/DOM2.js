f022d68cd93ccc9b280a025d45bb3db1be2f2adc
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-05-03 14:24:59 +0700

    Latihan DOM 2

--- Week - 3/DOM2.js
+++ Week - 3/DOM2.js
@@ -1,8 +1,7 @@
 let tombol = document.getElementById("gantiWarna");
 let warna = ["lightblue", "lightgreen", "lavender", "salmon"];
-let acak = Math.floor(Math.random() * warna.length);
 
 tombol.addEventListener("click", function() {
-    // document.body.style.backgroundColor = "lightblue";
-    document.body.style.backgroundColor = warna[acak];
+  let acak = Math.floor(Math.random() * warna.length);
+  document.body.style.backgroundColor = warna[acak];
 });
